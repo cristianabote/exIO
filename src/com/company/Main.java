88a6266f36febc497b4cd48c7849562adec6ef06
cristianabote <cristianamihaelaplesa@gmail.com>
2021-04-08 09:51:55 +0300

    add .gitignore file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException{
         // Creeaza un program care citeste numerele din cele 3 fisiere atasate.Dupa citire , aceste numere vor fi sortate in
         // ordine crescatoare si vor fi scrise intr-un fisier nou denumit "rezultat.txt". Pe prima linie se va scrie cel
-        // mai mare numar dintre cele citite, urmat numarul total de elemente citite din cele 3 fisiere. (raspuns : 991 44)
+        // mai mare numar dintre cele citite, urmat numarul total de elemente citite din cele 3 fisiere. (raspuns : 991 46)
         String filePath = "C:\\Cristiana\\curs\\PLATFORMA IT\\ProiecteJava\\exIO\\";
         String fileName = "rezultat.txt";
         File finalFile = new File(filePath + fileName);
